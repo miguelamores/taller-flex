f1c68d44fd6ee688e29728da2dd6905303aca2d6
Miguel Amores <migueamores@gmail.com>
2025-01-15 12:13:43 -0500

    wrap function with useCallback to create function just one

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export function useTransactions() {
   const [transactions, setTransactions] = useState<Transaction[]>([
@@ -10,25 +10,27 @@ export function useTransactions() {
   const [target, setTarget] = useState<number | null>(null);
   const [result, setResult] = useState<string>("");
 
-  const handleCheckTransactions = ({
-    targetAmount,
-  }: {
-    targetAmount: number | null;
-  }) => {
-    if (targetAmount === null) return;
+  const handleCheckTransactions = useCallback(
+    ({ targetAmount }: { targetAmount: number | null }) => {
+      if (targetAmount === null) return;
 
-    for (let i = 0; i < transactions.length; i++) {
-      for (let j = i + 1; j < transactions.length; j++) {
-        if (transactions[i].amount + transactions[j].amount === targetAmount) {
-          setResult(
-            `Transactions ${transactions[i].id} and ${transactions[j].id} add up to ${targetAmount}`
-          );
-          return;
+      for (let i = 0; i < transactions.length; i++) {
+        for (let j = i + 1; j < transactions.length; j++) {
+          if (
+            transactions[i].amount + transactions[j].amount ===
+            targetAmount
+          ) {
+            setResult(
+              `Transactions ${transactions[i].id} and ${transactions[j].id} add up to ${targetAmount}`
+            );
+            return;
+          }
         }
       }
-    }
-    setResult("No matching transactions found.");
-  };
+      setResult("No matching transactions found.");
+    },
+    []
+  );
 
   return {
     target,
